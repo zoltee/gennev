5c7e0ee5065564cd8d0466aa902fc6131d609490
Zoltan <zoltee@gmail.com>
2023-01-25 14:08:35 -0800

    Update connection to hosted backend

--- backend/src/config.php
+++ backend/src/config.php
@@ -8,8 +8,8 @@ const DB_SERVER = 'localhost';
 const DB_NAME = 'gennev';
 
 //PATH
-const PATH_SRC_ROOT = 'z:/work/gennev-community-pages/backend/src/';
-const DATA_FILE= 'z:/work/gennev-community-pages/backend/data/sample-data.txt';
+const PATH_SRC_ROOT = '/opt/bitnami/apache/htdocs/src/';
+const DATA_FILE= '/opt/bitnami/apache/htdocs/data/sample-data.txt';
 
 //PAGINATION
 const DEFAULT_ITEMS_PER_PAGE = 10;
